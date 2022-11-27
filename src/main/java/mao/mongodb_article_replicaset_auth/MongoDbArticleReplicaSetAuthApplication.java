064437e06cf0ce0c7f7b1729f2f6f0c02824c4c9
mao <1296193245@qq.com>
2022-11-27 14:56:11 +0800

    MongoDB_article_replicaSet_auth

---
+++ src/main/java/mao/mongodb_article_replicaset_auth/MongoDbArticleReplicaSetAuthApplication.java
@@ -0,0 +1,15 @@
+package mao.mongodb_article_replicaset_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbArticleReplicaSetAuthApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MongoDbArticleReplicaSetAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mao/mongodb_article_replicaset_auth/MongoDbArticleReplicaSetAuthApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.mongodb_article_replicaset_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbArticleReplicaSetAuthApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
